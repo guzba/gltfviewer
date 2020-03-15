2cb09f7ef9a1d086104bd1acaa0f249db96675e6
Ryan Oldenburg <guzba8@gmail.com>
2020-03-15 16:18:44 -0500

    move camera

--- src/gltfviewer.nim
+++ src/gltfviewer.nim
@@ -9,6 +9,11 @@ var
   activeModel: int
   shader: GLuint
   model: Model
+  mousePos, mousePosPrev, mouseDelta: Vec2
+  mouseClicked: bool
+  buttonDown = newSeq[bool](348)
+  cameraHpr = vec3(0, PI/2, 0)
+  cameraPos = vec3(0, 0, 400)
   view, proj: Mat4
 
 for kind, path in walkDir("models"):
@@ -35,16 +40,25 @@ proc onKey(
   window: Window,
   key, scancode, action, modifiers: cint
 ) {.cdecl.} =
+  if action == RELEASE:
+    buttonDown[key] = false
+    return
+
   if action != PRESS:
     return
+
   if key == KEY_ESCAPE:
     window.setWindowShouldClose(1)
     return
+
+  buttonDown[key] = true
 
   if key == KEY_LEFT:
     activeModel = max(activeModel - 1, 0)
   elif key == KEY_RIGHT:
     activeModel = min(activeModel + 1, len(models) - 1)
+  else:
+    return
 
   model.clearFromGpu()
   model = loadModel(joinPath(
@@ -52,7 +66,17 @@ proc onKey(
   ))
   model.uploadToGpu()
 
+proc onMouseButton(
+  window: Window,
+  button, action, modifiers: cint
+) {.cdecl.} =
+  if action == RELEASE:
+    mouseClicked = false
+  elif action == PRESS:
+    mouseClicked = true
+
 discard window.setKeyCallback(onKey)
+discard window.setMouseButtonCallback(onMouseButton)
 
 loadExtensions()
 
@@ -85,10 +109,37 @@ while windowShouldClose(window) == 0:
   glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
   glUseProgram(shader)
 
-  view = lookAt(vec3(0, 5, 1000), vec3(0, 0, 0), vec3(0, 1, 0)) # translate(vec3(0, 0, -10))
-  proj = perspective(45, framebufferWidth / framebufferHeight, 0.1, 10000)
+  block:
+    var x, y: float64
+    window.getCursorPos(addr x, addr y)
+    mousePos = vec2(x, y)
+    mouseDelta = mousePos - mousePosPrev
+    mousePosPrev = mousePos
 
-  # where does shader actually go?
+  let fov = (
+    rotateX(cameraHpr.y) * rotateZ(cameraHpr.x) * translate(vec3(1, 0, 0))
+  ).inverse()
+
+  const moveSpeed = -0.25
+  if buttonDown[KEY_W]:
+    cameraPos = cameraPos + fov.fov * moveSpeed
+  if buttonDown[KEY_S]:
+    cameraPos = cameraPos - fov.fov * moveSpeed
+  if buttonDown[KEY_D]:
+    cameraPos = cameraPos - fov.right * moveSpeed
+  if buttonDown[KEY_A]:
+    cameraPos = cameraPos + fov.right * moveSpeed
+  if buttonDown[KEY_E]:
+    cameraPos = cameraPos - fov.up * moveSpeed
+  if buttonDown[KEY_Q]:
+    cameraPos = cameraPos + fov.up * moveSpeed
+
+  cameraHpr.x -= mouseDelta.x / 300
+  cameraHpr.y -= mouseDelta.y / 300
+
+  view = rotateX(cameraHpr.y) * rotateZ(cameraHpr.x) * translate(-cameraPos)
+  proj = perspective(45, framebufferWidth / framebufferHeight, 0.1, 1000)
+
   model.draw(shader, view, proj)
 
   var error: GLenum
